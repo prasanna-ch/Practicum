f44264330afa3db1060363505a08bd445896e7d7
prasanna-ch <lakshmiprasannachintalapudi@msitprogram.net>
2020-06-18 19:50:43 +0530

    prjct

--- Firebase/GloStyles.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet, Platform } from "react-native";
-export default StyleSheet.create({
-  droidSafeArea: {
-    flex: 1,
-    backgroundColor: "white",
-    paddingTop: Platform.OS === "android" ? 30 : 0,
-  },
-});
